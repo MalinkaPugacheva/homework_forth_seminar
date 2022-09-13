d72c2f017786c183411e6b5609c78dac87aada1e
Malinka <alina-malakhovich@rambler.ru>
2022-09-13 20:49:40 +0300

    Done/only first

--- forth_homework/fifth_homework/sixth_homework/Program.cs
+++ forth_homework/fifth_homework/sixth_homework/Program.cs
@@ -1,81 +1,50 @@
 ﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+/*
+Console.Clear();
 
-int Comparison(int[](arrayNumber)
+Console.Write($"Input number М(the amount of numbers): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+       Console.Write($"Input {i+1} number: ");
+       massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Comparison(int[] massiveNumbers)
 {
   int count = 0;
-  for (int i = 0; i <(arrayNumber.Length; i++)
+  for (int i = 0; i < massiveNumbers.Length; i++)
   {
-    if(arrayNumber[i] > 0 ) count += 1; 
+    if(massiveNumbers[i] > 0 ) count += 1; 
   }
   return count;
 }
-void EnterNum(int m)
-{
-    int[](arrayNumber = new int[m];
-for (int i = 0; i < m; i++)
-  {
-    Console.Write($"Input {i+1} number: ");
-(arrayNumber[i] = Convert.ToInt32(Console.ReadLine());
-  }
-}
 
-Console.Write($"Input number М(size): ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[](arrayNumber = new int[m];
+InputNumbers(m);
 
-
-EnterNum(m);
-
-Console.WriteLine($"Введено чисел больше 0: {Comparison(arrayNumber)} ");
-
-// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine($"The amount of numbers more than 0: {Comparison(massiveNumbers)} ");
+*/
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
-/*
 
-Console.WriteLine($"\nЗадача 43.  Найти точку пересечения двух прямых \n");
-
-double[,] coeff = new double[2, 2];
-double[] crossPoint = new double[2];
-
-void InputCoefficients(){
-  for (int i = 0; i < coeff.GetLength(0); i++)
+void WriteRatio()
+{
+  for (int i = 0; i < ratio.GetLength(0); i++)
   {
-    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
-    for (int j = 0; j < coeff.GetLength(1); j++)
+    Console.Write($"Input ratio {i+1} first equation (y = k * x + b):\n");
+    for (int j = 0; j < ratio.GetLength(1); j++)
     {
-      if(j==0) Console.Write($"Введите коэффициент k: ");
-      else Console.Write($"Введите коэффициент b: ");
-      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+      if(j==0) Console.Write($"Input ratio k: ");
+      else Console.Write($"Input ratio b: ");
+      ratio[i,j] = Convert.ToInt32(Console.ReadLine());
     }
   }
 }
-
-double[] Decision(double[,] coeff)
-{
-  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
-  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
-  return crossPoint;
-}
-
-void OutputResponse(double[,] coeff)
-{
-  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
-  {
-    Console.Write($"\nПрямые совпадают");
-  }
-  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
-  {
-    Console.Write($"\nПрямые параллельны");
-  }
-  else 
-  {
-    Decision(coeff);
-    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
-  }
-}
-
-InputCoefficients();
-OutputResponse(coeff);
-*/
